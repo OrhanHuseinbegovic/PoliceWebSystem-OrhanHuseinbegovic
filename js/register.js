8295ff4789ae127b6b2506ed7277e86b0d388376
Orhan Huseinbegovic <orhan.huseinbegovic@stu.ibu.edu.ba>
2024-05-13 23:04:28 +0200

    Registration fix

--- js/register.js
+++ js/register.js
@@ -3,7 +3,9 @@ $("#registerForm").validate({
     rules: {
         personalID:{
             required: true,
-            number: true
+            number: true,
+            minlength: 8,
+            maxlength: 8
         },
         name:{
             required: true
@@ -16,7 +18,9 @@ $("#registerForm").validate({
         },
         phone:{
             required: true,
-            number: true
+            number: true,
+            minlength: 8,
+            maxlength: 8
         },
         password:{
             required: true,
@@ -30,7 +34,9 @@ $("#registerForm").validate({
     messages: {
         personalID:{
             required: "Please enter your personal ID",
-            number: "Please enter a valid personal ID"
+            number: "Please enter a valid personal ID",
+            minlength: "Personal ID must be 8 characters long",
+            maxlength: "Personal ID must be 8 characters long"
         },
         name:{
             required: "Please enter your name"
@@ -43,7 +49,9 @@ $("#registerForm").validate({
         },
         phone:{
             required: "Please enter your phone number",
-            number: "Please enter a valid phone number"
+            number: "Please enter a valid phone number",
+            minlength: "Phone number must be 8 characters long",
+            maxlength: "Phone number must be 8 characters long"
         },
         password:{
             required: "Please enter your password",
@@ -56,14 +64,90 @@ $("#registerForm").validate({
     },
     submitHandler: function(form, event) {
         event.preventDefault();
-        blockUi("#registerForm"); 
-        let data = serializeForm(form);
-        console.log(JSON.stringify(data));
-        $("#registerForm")[0].reset();
-        unblockUi("#registerForm");
-    }
+        
+        FormValidation.validate("#registerForm",{}, function(data){
+            Utils.block_ui("#registerForm");
+        
+            // Get the value of the checkbox
+            var checkboxValue = $("#isAdmin").is(":checked") ? 1 : 0;
+            
+            var email = data['email'];
+        
+            for (var i = 0; i < email.length; i++) {
+                var char = email.charAt(i);
+                var nextChar = email.charAt(i+1);
+                if(char == 'č' || char == 'ć'){
+                    email = email.replace(char, 'c');
+                }
+                else if(char == 'š'){
+                    email = email.replace(char, 's');
+                }
+                else if(char == 'ž'){
+                    email = email.replace(char, 'z');
+                }  
+                else if (char == 'đ'){
+                    email = email.replace(char, 'd');
+                }
+                else if(char == 'd' && nextChar == 'ž'){
+                    email = email.replace(char+nextChar, 'dz');
+                }
+            }
+        
+            data['email'] = email;
+        
+            // Add the checkbox value to the data object
+            data['isAdmin'] = checkboxValue;
+        
+        
+            var password = data['password'];
+            var email = data['email'];
+        
+            console.log("Data from form is serialized into", data);
+            $.post(Constants.API_BASE_URL + "officers/add", data)
+                .done(function(data){
+                    $("#registerForm")[0].reset();
+                    document.getElementById("confirmationModal");
+        
+                    document.getElementById("userEmail").textContent = email;
+                    document.getElementById("userPassword").textContent = password;
+        
+                    //ili
+        
+                    //$('#userEmail').text(email);
+                    //$('#userPassword').text(password);
+        
+                    // Show confirmation modal
+                    $("#confirmationModal").modal("show");
+        
+                    toastr.success("User successfully registered");
+        
+                    $("#closeModalBtn, .close").click(function() {
+                        $("#confirmationModal").modal("hide");
+                    });
+                    
+                })
+                .fail(function(error){
+                    toastr.error("Error while registering user");
+                })
+                .always(function(){
+                    Utils.unblock_ui("#registerForm");
+                    
+                }
+            );
+        })
 });
+/*
 
+serializeForm = (form) => {
+    let jsonResul = {};
+    $.each($(form).serializeArray(), function () {
+        jsonResul[this.name] = this.value;    
+    });
+    return jsonResul;
+}
+
+
+/*
 blockUi = (element) => {
     $(element).block({
       message: '<div class="spinner-border text-primary" role="status"></div>',
@@ -81,14 +165,6 @@ blockUi = (element) => {
 unblockUi = (element) => {
     $(element).unblock({});
 };  
-
-serializeForm = (form) => {
-    let jsonResul = {};
-    $.each($(form).serializeArray(), function () {
-        jsonResul[this.name] = this.value;    
-    });
-    return jsonResul;
-}
 
 
 getUsers = () => {
