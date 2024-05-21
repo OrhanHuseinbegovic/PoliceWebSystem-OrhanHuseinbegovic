82f185494f3cfbf0ef2053c4da7b138386fc8c21
Orhan Huseinbegovic <orhan.huseinbegovic@stu.ibu.edu.ba>
2024-05-21 22:48:00 +0200

    milestone 5 database and deployment

--- backend/rest/routes/auth_routes.php
+++ backend/rest/routes/auth_routes.php
@@ -69,7 +69,7 @@ Flight::group('/auth', function(){
 
         $token = JWT::encode(
             $jwt_payload,
-            JWT_SECRET,
+            Config::JWT_SECRET(),
             'HS256'
         );
 
@@ -100,7 +100,7 @@ Flight::group('/auth', function(){
             if(!$token)
                 Flight::halt(401, "Missing authentication header");
 
-            $decoded_token = JWT::decode($token, new Key(JWT_SECRET, 'HS256'));
+            $decoded_token = JWT::decode($token, new Key(Config::JWT_SECRET(), 'HS256'));
 
             Flight::json([
                 'jwt_decoded' => $decoded_token,
